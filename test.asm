a255fbd0ec11946216a4970b1e174089202bee87
Meidi L <135418944+Studyent@users.noreply.github.com>
2024-06-09 12:31:59 +0200

    Add files via upload

---
+++ test.asm
@@ -0,0 +1,39 @@
+;Réalisé avec NASM
+
+
+[bits 16] ; Précise le Mode réel 16 bits
+[org 0x7c00] ; démarre à l'addresse 0x7c00 après que la valeur 0xaa55 soit trouvé dans les 2 derniers
+; bits du bootloader 
+
+global _start
+
+section .text
+
+_start:
+
+    xor ax,ax
+    mov ds,ax
+    mov es,ax
+    mov si,msg
+    call print 
+    jmp $ ; pas de code d'initialisation indésirable
+
+
+
+print:
+    mov al,[si] ; copie
+    inc si
+    cmp al,0
+    je done
+    mov ah,0x0e
+    int 0x10
+    jmp print
+
+done:
+    jmp $ ; boucle effectué->boucle sur la meme addresse
+
+
+    msg:
+        db "Hello, World !",0; Chaque byte en code ASCII + 0 pour fin de chaine
+        times 510-($-$$) db 0 ; remplir de 0 l'espace restant entre l'addresse courante $ et l'addresse de début $$
+        dw 0xaa55 ; ajout aux 2 derniers bits la valeur 0xaa55
